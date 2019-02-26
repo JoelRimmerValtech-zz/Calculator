81e92d4401e7d1305bc3fbde51a916b3b1b972a0
Joel Rimmer <joel.rimmer@valtech.com>
2019-02-26 12:59:02 +0000

    working calculator

--- src/joel/calculator/Main.java
+++ src/joel/calculator/Main.java
@@ -1,8 +1,34 @@
 package joel.calculator;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+        int randomNumber2 = random.nextInt(100);
+
+        System.out.println("First random number is: " + randomNumber);
+        System.out.println("Second random number is: " + randomNumber2);
+
+        System.out.println("Insert an operation of either '+', '-' or '*'");
+
+        Scanner scanner = new Scanner(System.in); String input = scanner.next();
+
+        if (input.equals("+")){
+            System.out.println("The answer is " + (randomNumber + randomNumber2));
+        }
+        else if (input.equals("-")){
+            System.out.println("The answer is " + (randomNumber - randomNumber2));
+        }
+        else if (input.equals("*")){
+            System.out.println("The answer is " + (randomNumber * randomNumber2));
+        }
+
+
+
     }
 }
